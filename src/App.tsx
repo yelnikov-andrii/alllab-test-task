ded94b2524f6953eaad3f2a1005abfb867fe9440
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-11-01 23:19:43 +0200

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import { JobDetailed } from './Components/JobDetailed/JobDetailed';
 function App() {
   const [jobs, setJobs] = React.useState([]);
   const [savedJob, setSavedJob] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     fetch('https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu')
       .then(response => {
         response.json()
           .then(res => {
             setJobs(res);
+            setIsLoading(false);
           })
-      })
+      });
   }, []);
 
   return (
     <div className="App">
       <Routes>
         <Route path="/jobs" element={
-          <JobList jobs={jobs} savedJob={savedJob} setSavedJob={setSavedJob}/>
+          <JobList jobs={jobs} savedJob={savedJob} setSavedJob={setSavedJob} isLoading={isLoading} />
         }>
         </Route>
         <Route path="/" element={
--- src/Components/JobList/JobList.tsx
+++ src/Components/JobList/JobList.tsx
@@ -2,14 +2,16 @@ import React, { Dispatch, SetStateAction} from 'react';
 import { Job } from '../../types/type';
 import { JobItem } from './JobItem';
 import { Pagination } from './Pagination';
+import ContentLoader from 'react-content-loader';
 
   type Props = {
     jobs: Job[];
     savedJob: string[];
     setSavedJob: Dispatch<SetStateAction<any>>;
+    isLoading: boolean;
   }
 
-export const JobList: React.FC <Props> = ({jobs, savedJob}) => {
+export const JobList: React.FC <Props> = ({jobs, savedJob, isLoading}) => {
   const [page, setPage] = React.useState(1);
   const jobsPerPage = 5;
   const lastElement = page * jobsPerPage;
@@ -23,6 +25,27 @@ export const JobList: React.FC <Props> = ({jobs, savedJob}) => {
   }
 
   const visibleContent = jobs.slice(firstElement, lastElement);
+
+  if (isLoading) {
+    return (
+      <div className='max-w-screen-xl w-full mx-auto'>
+        <ContentLoader 
+          speed={2}
+          width={1200}
+          height={700}
+          viewBox="0 0 600 500"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="67" y="53" rx="0" ry="0" width="1200" height="52" /> 
+          <rect x="68" y="116" rx="0" ry="0" width="1200" height="52" /> 
+          <rect x="69" y="183" rx="0" ry="0" width="1200" height="52" /> 
+          <rect x="69" y="250" rx="0" ry="0" width="1200" height="52" /> 
+          <rect x="72" y="319" rx="0" ry="0" width="1200" height="52" />
+        </ContentLoader>
+      </div>
+    )
+  }
 
   return (
     <section className='bg-slate-200 pt-7 pb-16'>
